ae80cd1544a32ada496e6ad18b49a9a85c77ddd2
HodgE <eathan.hodgkinson@aggiemail.usu.edu>
2020-10-27 15:50:27 -0600

    Added script to change title and began working on first div element.

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
 /* TODO: Build up the HTML document by using JavaScript DOM manipulation functions.
  * Do not rely on changes you may have made to index.html because the grader won't use that file.
  */
+document.title = "Interactive JavaScript Calculator";
+
+var UIDiv = document.createElement('div');
+UIDiv.setAttribute('class', 'black stuff-box shadowed');
+
+var header = document.createElement('h1');
+header.textContent = "JavaScript Calculator";
+
+UIDiv.appendChild(header);
+
+document.body.appendChild(UIDiv);
